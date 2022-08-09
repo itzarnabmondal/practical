938fde2c1efa65256d9959f263baf3434bdaefcc
Arnab Mondal <110527370+arxmondal@users.noreply.github.com>
2022-08-09 17:21:56 +0530

    Create ArithmeticOperations.java

---
+++ ArithmeticOperations.java
@@ -0,0 +1,13 @@
+public class ArithmeticOperations {
+	public static void main(String[] args) {
+		
+		int num1 = 100;
+		int num2 = 10;
+
+		System.out.println("\nTwo number is "+num1+" & "+num2+"\n");
+		System.out.println("Addition is: "+(num1+num2));
+		System.out.println("Subtraction is: "+(num1-num2));
+		System.out.println("Multiplication is: "+(num1*num2));
+		System.out.println("Division is "+(num1/num2));
+	}
+}
