08887a0f964b5b1d4eddb86bc72599c0c98323df
Arnab Mondal <110527370+arxmondal@users.noreply.github.com>
2022-08-10 15:22:21 +0530

    Create  AreaAndPerimeterOfSquare.java

---
+++ CoreJava/BasicPrograms/AreaAndPerimeterOfSquare.java
@@ -0,0 +1,7 @@
+public class AreaAndPerimeterOfSquare {
+   public static void main(String[] args) {
+      float s = 5.5F;
+      System.out.println("\nArea is: " +(s*s));
+      System.out.println("\nPerimeter = "+(4*s));
+   }
+}
