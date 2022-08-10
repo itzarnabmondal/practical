9f3eb15c2361f209e32b5cf76eaa9fd3f5b90730
Arnab Mondal <110527370+arxmondal@users.noreply.github.com>
2022-08-10 15:28:47 +0530

    Create AreaAndPerimeterOfRectangle.java

---
+++ CoreJava/BasicPrograms/AreaAndPerimeterOfRectangle.java
@@ -0,0 +1,7 @@
+ public class AreaAndPerimeterOfRectangle {
+     public static void main(String[] args) {
+         int l=10, b=20, perimeter, area;
+         System.out.println("Perimeter of rectangle:"+(2 * (l + b)));
+         System.out.println("Area of rectangle:"+(l * b));
+   }
+ }
