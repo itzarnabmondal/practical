d0c699fe3e3e6694e167c75bdb472d7d9fb80967
Arnab Mondal <110527370+arxmondal@users.noreply.github.com>
2022-08-10 15:34:13 +0530

    Update AreaAndPerimeterOfSquare.java

--- CoreJava/BasicPrograms/AreaAndPerimeterOfSquare.java
+++ CoreJava/BasicPrograms/AreaAndPerimeterOfSquare.java
@@ -1,7 +1,7 @@
 public class AreaAndPerimeterOfSquare {
    public static void main(String[] args) {
       float s = 5.5F;
-      System.out.println("\nArea is: " +(s*s));
-      System.out.println("\nPerimeter = "+(4*s));
+      System.out.println("\nArea is: " +(s*s)); // Area of square is side * side
+      System.out.println("\nPerimeter = "+(4*s)); // Perimeter of square is 4 * side
    }
 }
