224fd9cde019c725ffd6d5137196c1c8328251d1
Arnab Mondal <110527370+arxmondal@users.noreply.github.com>
2022-08-17 14:46:00 +0530

    Create Operators.java

---
+++ CoreJava/BasicPrograms/Operators.java
@@ -0,0 +1,20 @@
+public class Operators {
+	public static void main(String[] args) {
+		int num1=10;
+		int num2=2;
+		// Arithmetic Operator
+		int addition = num1+num2;
+        System.out.ptintln(addition);
+		// Assignment Operators
+		num1=12;
+        System.out.ptintln("Now value of num1: "+num1);
+		// Comparison Operators
+		if (num1>num2) {
+			System.out.println("True");
+		}
+		// Logical Operators
+		if (num1>num2 && num1 < 100) {
+			System.out.println("True");
+		}
+	}
+}
