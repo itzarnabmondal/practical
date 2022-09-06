23f9098e6c2fef6513908cef06d0517b93e08554
Arnab Mondal <110527370+arxmondal@users.noreply.github.com>
2022-09-06 20:16:07 +0530

    Create Constructor.java

---
+++ CoreJava/ObjectOrientedProgramming/Constructor.java
@@ -0,0 +1,9 @@
+public class Constructor {
+    Bike1(){
+        System.out.println("Bike is created");
+    }  
+    //main method  
+    public static void main(String args[]){
+        Bike1 b=new Bike1();  
+    }  
+}
