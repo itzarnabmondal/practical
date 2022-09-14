b096634b615d91888c56e04ad4edff749f284057
Arnab Mondal <arxxxmondal@gmail.com>
2022-09-14 20:19:17 +0530

    Update and rename MethodOverloading.java to MethodOverloading1.java

--- CoreJava/ObjectOrientedProgramming/MethodOverloading1.java
+++ CoreJava/ObjectOrientedProgramming/MethodOverloading1.java
@@ -2,11 +2,11 @@ class Adder{
     static int add(int a,int b){
         return a+b;
     }  
-    static int add(int a,int b,int c){ // same name method with different parameter
+    static int add(int a,int b,int c){ // same name method with different number of aurgument.
         return a+b+c;
     }  
 }  
-public class MethodOverloading {
+public class MethodOverloading1 {
     public static void main(String[] args){
         System.out.println(Adder.add(11,11));
         System.out.println(Adder.add(11,11,11));
