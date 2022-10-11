6ed46a4b2d9264247986eb46f107192842f30b88
Arnab Mondal <arxxxmondal@gmail.com>
2022-10-11 21:48:21 +0530

    Create PrintArray.java

---
+++ CoreJava/Array/PrintArray.java
@@ -0,0 +1,13 @@
+public class PrintArray {
+  public static void main(String args[]) {
+    int arr[] = {
+      33,
+      3,
+      4,
+      5
+    };
+    //printing array using for-each loop  
+    for (int i: arr)
+      System.out.println(i);
+  }
+}
